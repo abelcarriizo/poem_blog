4f8c65ac0c0873199039f3bffee7e78ff1a50849
abelcarriizo <abelcarriizo@outlook.com>
2024-06-25 20:39:47 -0300

    Inicializacion de recursos actualizada

--- src/resources/__init__.py
+++ src/resources/__init__.py
@@ -2,3 +2,6 @@ from .user import User as UserResource
 from .user import Users as UsersResource
 from .poem import Poem as PoemResource
 from .poem import Poems as PoemsResource
+from .comment import Comment as CommentResource
+from .comment import Comments as CommentsResource
+
