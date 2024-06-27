2807d0dd3df143562795b0b3b99556156c96ac16
abelcarriizo <abelcarriizo@outlook.com>
2024-06-27 20:26:30 -0300

    Recurso usuario actualizado. Añadido "get" y "post"

--- src/resources/user.py
+++ src/resources/user.py
@@ -1,11 +1,12 @@
 from .. import db
 from src.models import userModel
-from flask import jsonify
+from flask import jsonify, request
 from flask_restful import Resource
 
 class User(Resource):
     def get(self, id):
         user = db.session.query(userModel).get_or_404(id)
+        print(user)
         return user.to_json()
 
 class Users(Resource):
@@ -13,3 +14,8 @@ class Users(Resource):
         users = db.session.query(userModel).all()
         return jsonify([user.to_json() for user in users])
         
+    def post(self):
+        user = userModel.from_json(request.get_json())
+        db.session.add(user)
+        db.session.commit()
+        return user.to_json(), 201
