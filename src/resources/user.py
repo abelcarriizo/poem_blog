08bc367b86d0391bf30a45d829b8c775d5a8df31
abelcarriizo <abelcarriizo@outlook.com>
2024-06-28 19:33:38 -0300

    Actualizado. Modelo "user" modificado

--- src/resources/user.py
+++ src/resources/user.py
@@ -1,16 +1,15 @@
 from .. import db
-from src.models import userModel
+from src.models import UserModel
 from flask import jsonify, request
 from flask_restful import Resource
 
 class User(Resource):
     def get(self, id):
-        user = db.session.query(userModel).get_or_404(id)
-        print(user)
+        user = db.session.query(UserModel).get_or_404(id)
         return user.to_json()
     
     def put(self, id):
-        user = db.session.query(userModel).get_or_404(id)
+        user = db.session.query(UserModel).get_or_404(id)
         data = request.get_json().items()
         for key, value in data:
             setattr(user, key, value)
@@ -19,18 +18,18 @@ class User(Resource):
         return user.to_json(), 201
     
     def delete(self, id):
-        user = db.session.query(userModel).get_or_404(id)
+        user = db.session.query(UserModel).get_or_404(id)
         db.session.delete(user)
         db.session.commit()
         return '', 204
 
 class Users(Resource):
     def get(self):
-        users = db.session.query(userModel).all()
+        users = db.session.query(UserModel).all()
         return jsonify([user.to_json() for user in users])
         
     def post(self):
-        user = userModel.from_json(request.get_json())
+        user = UserModel.from_json(request.get_json())
         db.session.add(user)
         db.session.commit()
         return user.to_json(), 201
