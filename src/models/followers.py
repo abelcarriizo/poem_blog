05384372f03c4d7865622af54e4f0080bdb75240
abelcarriizo <abelcarriizo@outlook.com>
2024-07-04 21:51:33 -0300

    Actualizado. Modelo "followers" modificado

---
+++ src/models/followers.py
@@ -0,0 +1,6 @@
+from .. import db
+
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'), primary_key=True)
+)
