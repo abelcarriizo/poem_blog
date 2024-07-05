fb87d4cbd67be897d400ad404d56db06fa90273a
abelcarriizo <abelcarriizo@outlook.com>
2024-07-04 21:52:16 -0300

    Actualizado. Modelo "poems" modificado

--- src/models/poems.py
+++ src/models/poems.py
@@ -1,23 +1,25 @@
 from .. import db
+from .likes import likes
 
 class Poem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120), nullable=False)
-    author = db.Column(db.Integer, nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     genre = db.Column(db.String(50), nullable=False)
     date_created = db.Column(db.DateTime, server_default=db.func.now(), nullable=False)
     description = db.Column(db.String(255)) #Descripcion breve del poema
     content = db.Column(db.Text, nullable=False) #Contenido extenso del poema
-
-    def __repr__(self):
-        return f'Titulo: {self.title}, Género: {self.genre}, Fecha: {self.date_created}, Descripción: {self.description}'
+    
+    author = db.relationship('User', back_populates='poems') 
+    comments = db.relationship('Comment', back_populates='poem', cascade='all, delete-orphan')
+    liked_by = db.relationship('User', secondary=likes, back_populates='liked_poems', lazy='dynamic')
     
     #Convertir objeto a JSON
     def to_json(self):
         poem_json = {
             "id": self.id,
             "title": self.title,
-            "author": self.author,
+            "author_id": self.author_id,
             "genre": self.genre,
             'date_created': self.date_created.isoformat(),
             "description": self.description,
@@ -26,12 +28,13 @@ class Poem(db.Model):
         return poem_json
 
     #Convertir JSON a objeto
+    @staticmethod
     def from_json(poem_json):
         id = poem_json.get("id")
         title = poem_json.get("title")
-        author = poem_json.get("author")
+        author_id = poem_json.get("author_id")
         genre = poem_json.get("genre")
         date_created = poem_json.get("date_created")
         description = poem_json.get("description")
         content = poem_json.get("content")
-        return Poem(id=id, title=title, author=author, genre=genre, date_created=date_created, description=description, content=content)
+        return Poem(id=id, title=title, author_id=author_id, genre=genre, date_created=date_created, description=description, content=content)
