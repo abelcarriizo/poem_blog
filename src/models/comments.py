463360d646a3f106e45136defd609022bc40906b
abelcarriizo <abelcarriizo@outlook.com>
2024-07-04 21:51:04 -0300

    Actualizado. Modelo "comments" modificado

--- src/models/comments.py
+++ src/models/comments.py
@@ -2,25 +2,31 @@ from .. import db
 
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    author = db.Column(db.Integer, nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    poem_id = db.Column(db.Integer, db.ForeignKey('poem.id'), nullable=False)
     date_created = db.Column(db.DateTime, server_default=db.func.now(), nullable=False)
     content = db.Column(db.String(255))
 
-    def __repr__(self) -> str:
-        return f'Autor: {self.author}, Contenido: {self.content}'
+    author = db.relationship('User', back_populates='comments')
+    poem = db.relationship('Poem', back_populates='comments')
     
+    #Convertir objeto a JSON
     def to_json(self):
         comment_json = {
             "id": self.id,
-            "author": self.author,
+            "author_id": self.author_id,
+            "poem_id": self.poem_id,
             "date_created": self.date_created.isoformat(),
             "content": self.content
         }
         return comment_json
 
+    #Convertir JSON a objeto
+    @staticmethod
     def from_json(comment_json):
         id = comment_json.get("id")
-        author = comment_json.get("author")
+        author_id = comment_json.get("author_id")
+        poem_id = comment_json.get("poem_id")
         date_created = comment_json.get("date_created")
         content = comment_json.get("content")
-        return Comment(id=id, author=author, date_created=date_created, content=content)
+        return Comment(id=id, author_id=author_id, poem_id=poem_id, date_created=date_created, content=content)
