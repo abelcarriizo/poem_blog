442c45700e6052f86f94b34ef8dc5b584568cd3b
abelcarriizo <abelcarriizo@outlook.com>
2024-07-04 21:53:10 -0300

    Actualizado. Paquete de inicializacion modificado

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,3 +1,5 @@
-from .users import User as UserModel
-from .poems import Poem as PoemModel
 from .comments import Comment as CommentModel
+from .followers import followers as followersTable
+from .likes import likes as likesTable
+from .poems import Poem as PoemModel
+from .users import User as UserModel
