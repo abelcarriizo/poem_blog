4a2442b85ea6520aa61545f50a9f77f205418a5c
abelcarriizo <abelcarriizo@outlook.com>
2024-07-09 20:09:37 -0300

    Actualizado. Se añadio paginacion al recurso "comment"

--- src/resources/comment.py
+++ src/resources/comment.py
@@ -24,9 +24,24 @@ class Comment(Resource):
         return '', 204
     
 class Comments(Resource):
-    def get(self):
-        comments = db.session.query(CommentModel).all()
-        return jsonify([comment.to_json() for comment in comments])
+    def get(self, poem_id):
+        page = request.args.get('page', 1, type=int)
+        per_page = request.args.get('per_page', 10, type=int)
+        
+        comments_pagination = db.session.query(CommentModel).filter_by(poem_id=poem_id).order_by(CommentModel.date_created).paginate(page=page, per_page=per_page, error_out=False)
+        
+        data = {
+            'total': comments_pagination.total,
+            'pages': comments_pagination.pages,
+            'current_page': comments_pagination.page,
+            'next_page': comments_pagination.next_num,
+            'prev_page': comments_pagination.prev_num,
+            'has_next': comments_pagination.has_next,
+            'has_prev': comments_pagination.has_prev,
+            'items': [comment.to_json() for comment in comments_pagination.items]
+        }
+        
+        return jsonify(data)
     
     def post(self):
         comment = CommentModel.from_json(request.get_json())
